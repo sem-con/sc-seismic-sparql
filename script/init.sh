99665b8395b1afe048d64e829605080585e93b28
Christoph Fabianek <christoph.fabianek@gmail.com>
2019-06-14 15:10:32 +0200

    adopt new srv-rml structure

--- script/init.sh
+++ script/init.sh
@@ -2,7 +2,8 @@
 
 rm -f /usr/src/app/tmp/pids/server.pid /usr/src/app/log/*.log
 rails server -b 0.0.0.0 &
-java -jar srv-rml-1.2.2-SNAPSHOT-jar-with-dependencies.jar -m /config/mapping.ttl -a "$1" -t json -o /config/ontology.ttl &
+/usr/src/app/script/parse_init.rb "$1"
+java -jar srv-rml-1.3.0-SNAPSHOT-jar-with-dependencies.jar -m /usr/src/app/config/mapping.ttl -c /usr/src/app/config/constraint.ttl -a "$(< /usr/src/app/config/data_url)" -t json -o /usr/src/app/config/ontology.ttl &
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:3000/api/active)" != "200" ]]; do sleep 5; done'
-/usr/src/app/script/init.rb "$2"
+/usr/src/app/script/init.rb "$1"
 sleep infinity
---
+++ script/parse_init.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'rdf'
+require 'linkeddata'
+
+SEMCON_ONTOLOGY = "http://w3id.org/semcon/ns/ontology#"
+
+init_input = ARGV.pop
+init = RDF::Repository.new()
+init << RDF::Reader.for(:trig).new(init_input.to_s)
+
+mapping = nil
+init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataMapping" ? mapping = g : nil }
+File.write('/usr/src/app/config/mapping.ttl', mapping.dump(:trig).strip.split("\n")[2..-2].join("\n").strip)
+
+ontology = nil
+init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataModel" ? ontology = g : nil }
+File.write('/usr/src/app/config/ontology.ttl', ontology.dump(:trig).strip.split("\n")[2..-2].join("\n").strip)
+
+constraint = nil
+init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataConstraint" ? constraint = g : nil }
+File.write('/usr/src/app/config/constraint.ttl', constraint.dump(:trig).strip.split("\n")[1..-2].join("\n").strip)
+
+base_config = nil
+init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "BaseConfiguration" ? base_config = g : nil }
+data_url = RDF::Query.execute(base_config) { pattern [:subject, RDF::URI.new(SEMCON_ONTOLOGY + "hasDataLocation"), :value] }.first.value.to_s.strip
+
+File.write('/usr/src/app/config/data_url', data_url.to_s)
