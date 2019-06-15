1433bd860d4ddce2607e3004f42ea2d3cb14f621
Christoph Fabianek <christoph.fabianek@gmail.com>
2019-06-15 09:14:06 +0200

    fix init.trig parsing

--- script/parse_init.rb
+++ script/parse_init.rb
@@ -11,11 +11,11 @@ init << RDF::Reader.for(:trig).new(init_input.to_s)
 
 mapping = nil
 init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataMapping" ? mapping = g : nil }
-File.write('/usr/src/app/config/mapping.ttl', mapping.dump(:trig).strip.split("\n")[2..-2].join("\n").strip)
+File.write('/usr/src/app/config/mapping.ttl', mapping.dump(:trig).strip.split("\n")[1..-2].join("\n").strip)
 
 ontology = nil
 init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataModel" ? ontology = g : nil }
-File.write('/usr/src/app/config/ontology.ttl', ontology.dump(:trig).strip.split("\n")[2..-2].join("\n").strip)
+File.write('/usr/src/app/config/ontology.ttl', ontology.dump(:trig).strip.split("\n")[1..-2].join("\n").strip)
 
 constraint = nil
 init.each_graph{ |g| g.graph_name == SEMCON_ONTOLOGY + "DataConstraint" ? constraint = g : nil }
